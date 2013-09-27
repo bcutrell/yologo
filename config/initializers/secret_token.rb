17b855997f40b8868954177af046d4803d759e9b
Ben Cutrell <bcutrell13@gmail.com>
2013-09-27 12:10:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yologo::Application.config.secret_key_base = 'b73cbcc485c67b21cfb984b0d2567deefb7bb589ef44e02a1741ff13ae063d7259e6b0468019d18cb8b616ef82f602618c747d9608c86a79a863a007aecca380'
