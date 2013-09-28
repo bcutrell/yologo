959de1b570f996d87d3223982e6344130bff1c21
Ben Cutrell <bcutrell13@gmail.com>
2013-09-28 10:14:44 -0400

    changed uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -36,6 +36,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [40, 40]
   end
 
+  version :medium do
+    process :resize_to_fill => [150, 150]
+  end
+
+
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
