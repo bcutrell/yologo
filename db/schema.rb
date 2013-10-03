57ae86b72453045f4b3ccfa29740929269f1c83a
Michael Swanson <mikeswanson12@msn.com>
2013-10-03 10:28:50 -0400

    adds category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131002183115) do
+ActiveRecord::Schema.define(version: 20131003141026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "logos", force: true do |t|
     t.string   "title",      null: false
--- spec/features/user_uploads_logo_spec.rb
+++ spec/features/user_uploads_logo_spec.rb
@@ -11,15 +11,16 @@ feature "user uploads logo", %Q{
   # * I must specify  required information by filling out a form:
   # * I must specify image upload path
   # * I must specify company name
-  # * If I don't specify all required information, 
+  # * I must specify logo category
+  # * If I don't specify all required information,
   #   I get an error;
-  # * If I specify the required information, 
+  # * If I specify the required information,
   #   the information is saved;
 
   scenario 'unauthenticated user attempts to upload logo' do
     visit new_logo_path
     expect(page).to have_content('You need to sign in or sign up before continuing.')
-  end 
+  end
 
   scenario 'authenticated user attempts to upload logo' do
     count = Logo.count
@@ -29,7 +30,7 @@ feature "user uploads logo", %Q{
     fill_in "Title", with: "Launch Academy"
     attach_file('logo_logo', "#{Rails.root}/spec/support/logo_images/launch_academy_logo.png")
     click_on "Create Logo"
-    
+
     expect(Logo.count).to eql(count +1)
     expect(current_path).to eql(root_path)
     expect(page).to have_content("Launch Academy")
@@ -43,10 +44,26 @@ feature "user uploads logo", %Q{
     sign_in_as(user)
     visit new_logo_path
     click_on "Create Logo"
-    
+
     expect(Logo.count).to eql(count)
     expect(page).to have_content("can't be blank")
   end
 
+  scenario 'user uploads logo with category' do
+    count = Logo.count
+    user = FactoryGirl.create(:user)
+    sign_in_as(user)
+    visit new_logo_path
+
+    fill_in"Title", with: "Launch Academy"
+    attach_file('logo_logo', "#{Rails.root}/spec/support/logo_images/launch_academy_logo.png")
+    select "Technology", from: "Sector"
+    click_on "Create Logo"
+
+    expect(Logo.count).to eql(count + 1)
+    expect(current_path).to eql(root_path)
+    expect(page).to have_content("Launch Academy")
+    expect(page).to have_content("Technology")
+  end
 end
 
