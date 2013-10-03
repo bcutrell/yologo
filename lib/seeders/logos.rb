c81aebdd485145793141eb5877c82ad161293dfa
Ben Cutrell <bcutrell13@gmail.com>
2013-10-03 15:48:44 -0400

    fixes seeder, pagination broken

--- lib/seeders/logos.rb
+++ lib/seeders/logos.rb
@@ -5,10 +5,10 @@ module Seeders
 
       def seed
         logos.each do |attributes|
-          # binding.pry
           logo = Logo.where(title: attributes[:title]).first
           if logo.nil?
             logo = Logo.new
+            logo.state = attributes[:state]
             logo.title = attributes[:title]
             image = File.open(File.join(Rails.root, attributes[:logo]))
             logo.logo = image
@@ -23,63 +23,78 @@ module Seeders
         [
           {
             title: "AIG",
-            logo: "/app/assets/images/aig.gif"
+            logo: "/app/assets/images/aig.gif",
+            state: "approved"
           },
           {
             title: "altria",
-            logo: "/app/assets/images/altria.gif"
+            logo: "/app/assets/images/altria.gif",
+            state: "approved"
           },
           {
             title: "Berkshire Hathaway",
-            logo: "/app/assets/images/berkshirehathaway.gif"
+            logo: "/app/assets/images/berkshirehathaway.gif",
+            state: "approved"
           },
           {
             title: "Chevron",
-            logo: "/app/assets/images/chevron.jpg"
+            logo: "/app/assets/images/chevron.jpg",
+            state: "approved"
           },
           {
             title: "Citi Group",
-            logo: "/app/assets/images/citigroup.gif"
+            logo: "/app/assets/images/citigroup.gif",
+            state: "approved"
           },
           {
             title: "Conoco Phillips",
-            logo: "/app/assets/images/conocophillips.gif"
+            logo: "/app/assets/images/conocophillips.gif",
+            state: "approved"
           },
           {
             title: "Exxon",
-            logo: "/app/assets/images/exxon.jpg"
+            logo: "/app/assets/images/exxon.jpg",
+            state: "approved"
           },
           {
             title: "Ford",
-            logo: "/app/assets/images/ford.gif"
+            logo: "/app/assets/images/ford.gif",
+            state: "approved"
           },
           {
             title: "General Electric",
-            logo: "/app/assets/images/general_electric.jpg"
+            logo: "/app/assets/images/general_electric.jpg",
+            state: "approved"
           },
           {
             title: "General Motors",
-            logo: "/app/assets/images/general_motors.gif"
+            logo: "/app/assets/images/general_motors.gif",
+            state: "approved"
           },
           {
             title: "Home Depots",
-            logo: "/app/assets/images/homedepot.gif"
+            logo: "/app/assets/images/homedepot.gif",
+            state: "approved"
           },
           {
             title: "HP",
-            logo: "/app/assets/images/hp.jpg"
+            logo: "/app/assets/images/hp.jpg",
+            state: "approved"
           },
           {
             title: "IBM",
-            logo: "/app/assets/images/ibm.jpg"
+            logo: "/app/assets/images/ibm.jpg",
+            state: "approved"
           },
           {
             title: "Verizon",
-            logo: "/app/assets/images/verizon.gif"
+            logo: "/app/assets/images/verizon.gif",
+            state: "approved"
           },
           {
             title: "Walmart",
-            logo: "/app/assets/images/walmart.gif"
+            logo: "/app/assets/images/walmart.gif",
+            state: "approved"
           },
         ]
       end
